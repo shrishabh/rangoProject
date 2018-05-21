8ed4d46ecb546f1e3d6418a5d5a7b65421cfff6a
shrishabh <rkhandelwal3@wisc.edu>
2018-05-21 17:44:13 -0500

    after the template chapter

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("This is the first one, done this too many times")
-
 
 # Create your views here.
+
+def index(request):
+    context_dict = {'boldmessage': "Just a message"}
+    rend = render(request,'rango/indx.html',context=context_dict)
+    #return HttpResponse("This is the first one, done this too many times<a href='/rango/about'>View about page</a>")
+    return rend
+
+def about(request):
+    return HttpResponse("Well, you know this is about page, right? <a href='/rango/'>View index page</a>")
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rango import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^rango/',include('rango.urls')),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
